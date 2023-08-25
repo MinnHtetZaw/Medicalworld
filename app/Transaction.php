0c1f934743cfc84697ff8751250a9a5c318a9352
MinnHtetZaw <100600961+MinnHtetZaw@users.noreply.github.com>
2023-08-25 11:41:11 +0630

    Update Transaction.php

--- app/Transaction.php
+++ app/Transaction.php
@@ -9,6 +9,7 @@ class Transaction extends Model
     //
     protected $fillable = [
         'bank_acc_id',
+        'account_id',
         'tran_date',
         'tran_time',
         'remark',
@@ -18,7 +19,9 @@ class Transaction extends Model
     public function bank_account() {
         return $this->belongsTo(BankAccount::class,'bank_acc_id');
     }
-    
+    public function account() {
+        return $this->belongsTo(Accounting::class,'account_id');
+    }
     public function order() {
         return $this->belongsTo(Order::class,'order_id');
     }
